37f5d3165b2eb4baae10c2db21b3d4c265646417
mwikalikyalo <winniemwikali07@gmail.com>
2023-03-03 09:26:10 +0300

    Add the different pages in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SelectComponent } from './select/select.component';
+import { PickComponent } from './pick/pick.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ThankyouComponent } from './thankyou/thankyou.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SelectComponent,
+    PickComponent,
+    ConfirmationComponent,
+    ThankyouComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
